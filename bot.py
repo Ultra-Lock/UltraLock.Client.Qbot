ff2f301af248a8efa8307827b7fd553da04419d2
Gaein_nidb <mail@gaein.cn>
2021-02-24 22:20:01 +0800

    更改if代码段

--- bot.py
+++ bot.py
@@ -12,18 +12,19 @@ bcc = Broadcast(loop=loop)
 app = GraiaMiraiApplication(
     broadcast=bcc,
     connect_info=Session(
-        host="http://localhost:8080", # 填入 httpapi 服务运行的地址
-        authKey="UltraLock.Client.Qbot", # 填入 authKey
-        account=2827539652, # 你的机器人的 qq 号
-        websocket=True # Graia 已经可以根据所配置的消息接收的方式来保证消息接收部分的正常运作.
+        host="http://localhost:8080",  # 填入 httpapi 服务运行的地址
+        authKey="UltraLock.Client.Qbot",  # 填入 authKey
+        account=2827539652,  # 你的机器人的 qq 号
+        websocket=True  # Graia 已经可以根据所配置的消息接收的方式来保证消息接收部分的正常运作.
     )
 )
 
+
 @bcc.receiver("FriendMessage")
-async def group_message_listener(app: GraiaMiraiApplication, group:Group):
-    if (app.getGroup(<Id>)==938624275):
-    await app.sendGroupMessage(Group, MessageChain.create([
-        Plain("Hello, World!")
-    ]))
+async def group_message_listener(app: GraiaMiraiApplication, group: Group):
+    if  group.Id == 938624275:
+        await app.sendGroupMessage(Group, MessageChain.create([
+            Plain("Hello, World!")
+        ]))
 
 app.launch_blocking()
